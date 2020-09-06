3e4d70c3d66364280fbd1af8ab00d645f44438a1
Unique Bhattarai <uniqgaming26@gmail.com>
2020-09-06 23:04:15 +0545

    some o the necessary thing to be asdded

--- product-display/src/Components/Navigation/Navigation.js
+++ product-display/src/Components/Navigation/Navigation.js
@@ -3,35 +3,33 @@ import './Navigation.css'
 
 const Navigation = () => {
     return (
-        <div>
-            <div class="container-fluid px-0">
+        <div class="container-fluid px-0">
     <header>
-        <div class="shape"></div> 
-        <nav class="navbar fixed-top navbar-expand-lg navbar-transparent"> <a class="navbar-brand" href="#">SPORT SHOES</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+        <div class="shape"></div>
+        <nav class="navbar fixed-top navbar-expand-lg navbar-transparent"> <a class="navbar-brand" href="https://i.imgur.com/jrRBTai.png">SPORT SHOES</a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"> <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a> </li>
-                    <li class="nav-item"> <a class="nav-link" href="#">MEN</a> </li>
-                    <li class="nav-item"> <a class="nav-link" href="#">WOMEN</a> </li>
-                    <li class="nav-item"> <a class="nav-link" href="#">KIDS</a> </li>
-                    <li class="nav-item"> <a class="nav-link" href="#">BOYS</a> </li>
-                    <li class="nav-item"> <a class="nav-link" href="#">GIRLS</a> </li>
+                    <li class="nav-item active"> <a class="nav-link" href="https://i.imgur.com/jrRBTai.png">HOME <span class="sr-only">(current)</span></a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="https://i.imgur.com/jrRBTai.png">MEN</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="https://i.imgur.com/jrRBTai.png">WOMEN</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="https://i.imgur.com/jrRBTai.png">KIDS</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="https://i.imgur.com/jrRBTai.png">BOYS</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="https://i.imgur.com/jrRBTai.png">GIRLS</a> </li>
                 </ul>
             </div>
         </nav> 
-                <div class="container">
-                <div class="row">
-                <div class="col-md-6 left-side"> <img src="https://i.imgur.com/jrRBTai.png" class="w-100" /> </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 left-side"> <img src="https://i.imgur.com/jrRBTai.png" alt="Shoes" class="w-100"/> </div>
                 <div class="col-md-6 right-side">
                     <h1>SPORTS SHOES</h1>
                     <p>FREE DELIVERY</p>
                     <div class="text-center"> <button class="btn order-button">ORDER NOW</button> </div>
                 </div>
-                </div>
-                 </div>
-                </header>
             </div>
         </div>
+    </header>
+</div>
     )
 }
 
